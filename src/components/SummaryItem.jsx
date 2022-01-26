9b5990bb1dfa5e5dadc49e3f553f0f1332456840
Bjorn Loyens <bjorn.loyens@gmail.com>
2022-01-26 09:05:24 +0100

    responsive

--- src/components/SummaryItem.jsx
+++ src/components/SummaryItem.jsx
@@ -3,14 +3,15 @@ import React from 'react';
 const SummaryItem = ({product,  incProducts, decProducts, removeProducts}) => {
   return (<><div key={product.id} className="checkout-summary-item">
   <img src={product.image} alt=""></img>
-  <div className="item-name">
+  <div className='item-info'><div className="item-name">
     <h3>{product.name}</h3>
     <p><span className="title">Aantal: </span>{product.count}<button onClick={()=>{incProducts(product.id)}} >+</button><button onClick={()=>{decProducts(product.id)}}>-</button></p>
     <p><span className="title">Eenheidsprijs: </span>€ {product.price}</p>
   </div>
   <div className="item-price">
-    <p className="title">{Number.parseFloat(product.count * product.price).toFixed(2)}</p>
-    <button onClick={()=>{removeProducts(product.id)}}>verwijder</button>
+    <p className="title">Subtotaal:€ {Number.parseFloat(product.count * product.price).toFixed(2)}</p>
+    <button onClick={()=>{removeProducts(product.id)}}>Verwijder</button>
+  </div>
   </div>
 </div>
 </>);
